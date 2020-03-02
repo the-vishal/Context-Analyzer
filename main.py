9d3e9411c75db30993233a7795ad35151455c2bb
Vishal <mail007tovishal@gmail.com>
2020-03-03 01:10:02 +0530

    changes in flask API: cosine similarity between sentence vectors

--- main.py
+++ main.py
@@ -2,28 +2,88 @@ import flask
 from flask import request, jsonify
 from flask_cors import CORS
 import json
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
 
+
+#FLASK CONFIG
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 cors = CORS(app)
 
 
+
+#HOME
 @app.route('/', methods=['GET'])
 def home():
     return "<h1><u>NLP based Context Analyzer</u></h1><p>This is a NLP based Context Analyzing tool. <br /> Developer(s) never take responsibility of any cases or results missed. <br /> It is just a tool proposed for helping searcher out there.</p>"
 
 
-# def apply
+#ANALYZE POST REQ API
 @app.route('/analyze', methods=['POST'])
 def analyze_context():
-    data = request.json.get('data', "")
-    input_data = request.json.get('test', "")
-    new_line = data.split('\n')
-    new_sentence = []
-    for i in new_line:
-        new_sentence.extend(i.split('.'))
+    article = request.json.get('article', "")
+    testData = request.json.get('testData', "")
+    context = CheckSimilarity(article=article, testdata=testData).relatedContext
+    return jsonify(context)
 
-    # print(new_sentence)
-    return jsonify([' dataset you can cluster it (for example using KMeans from scikit learn) after obtaining the representation, and before predicting on new da'])
 
+
+#COSINE SIMILARITY CHECK
+class CheckSimilarity(object):
+    def __init__(self, article, testdata):
+        self.article = article
+        self.testdata = testdata
+
+        self.stopWords = stopwords.words('english')+ list('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
+        self.stopWords = [' '+i+' ' for i in self.stopWords]                                          
+
+    @staticmethod
+    def tokenize_sentences(self, text):
+        sentences = []
+        lines = text.split('\n')
+        for i in lines:
+            sentences.extend(i.split('.'))
+        sentences = [i for i in sentences if i.strip()]
+        return sentences
+
+
+    def text_clean(self, text):
+        text = ' '+text.lower()+' '
+        for sw in self.stopWords:
+            text = text.replace(sw, " ")
+        return text.strip()
+
+
+    @staticmethod
+    def cossim_vectors(self, v1,v2):
+        v1 = v1.reshape(1,-1)
+        v2 = v2.reshape(1,-1)
+        
+        return cosine_similarity(v1,v2)[0][0]
+
+    @property
+    def relatedContext(self):
+        sentences = tokenize_sentences(self.article)
+        cleaned_sentences = list(map(self.text_clean, sentences))
+
+        test_sentences = tokenize_sentences(self.testdata)
+        cleaned_test_sentences = list(map(self.text_clean, test_sentences))
+
+        vectorizer = CountVectorizer().fit_transform(cleaned_test_sentences+cleaned_sentences)
+        vectors = vectorizer.toarray()
+
+        related_context = []
+
+        test_vector = vectors[0]
+        for index, vector in enumerate(vectors[1:]):
+            cos_sim = cossim_vectors(test_vector, vector)
+            if cos_sim>0.4:
+                related_context.append(sentences[index])
+        return related_context
+
+
+
+#APP RUNSERVER
 app.run()
