c173a5a2aaf6d9b8b7a70e18f8fb3ecdcd58a533
xhunt3r <vikasgupta.92155@gmail.com>
2020-03-03 23:19:21 +0530

    its working check the api and response of the api

--- content.js
+++ content.js
@@ -1,38 +1,37 @@
-alert("Generating summary highlights. This may take up to 30 seconds depending on length of article.");
+function summarize(train_data){
+	alert("Generating summary highlights. This may take up to 30 seconds depending on length of article.");
+	function unicodeToChar(text) {
+		return text.replace(/\\u[\dA-F]{4}/gi, 
+		      function (match) {
+		           return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+		      });
+	}
 
-console.log("1,",text)
-function unicodeToChar(text) {
-	return text.replace(/\\u[\dA-F]{4}/gi, 
-	      function (match) {
-	           return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
-	      });
+	// capture all text
+	var textToSend = document.body.innerText;
+	var settings = {
+	  "async": true,
+	  "crossDomain": true,
+	  "url": "http://127.0.0.1:5000/analyze",
+	  "method": "POST",
+	  "headers": {
+	    "content-type": "application/json"
+	  },
+	  "article": JSON.stringify(train_data),
+	  "test_data": JSON.stringify(target_data)
+	}
+
+	$.ajax(settings).done(function (response) { 
+		$.each(response, function( index, value ) {
+			value = unicodeToChar(value).replace(/\\n/g, '');
+			document.body.innerHTML = document.body.innerHTML.split(value).join('<span style="background-color: #fff799;">' + value + '</span>');
+		});
+	 })
+
 }
 
-// capture all text
-var textToSend = document.body.innerText;
 
-// console.log(textToSend);
-// summarize and send back
-var api_url = 'http://127.0.0.1:5000/analyze';
-	// let userinput = select('#userinput');
-	// console.log(userinput);
-var post_data = {
-					"data":textToSend,
-				};
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "http://127.0.0.1:5000/analyze",
-  "method": "POST",
-  "headers": {
-    "content-type": "application/json"
-  },
-  "data": JSON.stringify(post_data)
-}
-
-$.ajax(settings).done(function (response) { 
-	$.each(response, function( index, value ) {
-		value = unicodeToChar(value).replace(/\\n/g, '');
-		document.body.innerHTML = document.body.innerHTML.split(value).join('<span style="background-color: #fff799;">' + value + '</span>');
-	});
- })
+window.addEventListener('load', function load(event){
+var createButton = document.getElementById('clickme');
+createButton.addEventListener('click', function() { summarize(document.getElementById('input_textbox').value); });
+})
